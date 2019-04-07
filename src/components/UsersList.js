d80db00e3410d6c0402a6b3faff16257cb6fbe10
Gurlabe <gurlabe72@gmail.com>
2019-04-07 13:54:43 -0700

    Table set up using sematic UI, table displays infomration accordingly

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,26 +1,59 @@
 import React, { Component } from "react";
+import { Container, Header, Table, Rating } from "semantic-ui-react";
+
+import "semantic-ui-css/semantic.min.css";
 import UserData from "../data/users.json";
+
 class UsersList extends Component {
   render() {
     return (
       <div>
-        <h1> Hey Joe! </h1>
-        {UserData.map((userDetail, index) => {
-          return (
-            <div>
-              <h1>{userDetail.id}</h1>
-              <p>
-                <ul>
-                  <li>{userDetail.name}</li>
-                  <li>{userDetail.email_md5}</li>
-                  <li>{userDetail.last_location.ip}</li>
-                  <li>{userDetail.last_location.location}</li>
-                </ul>
-              </p>
-            </div>
-          );
-        })}
-        }{" "}
+        <Container fluid>
+          <h1> Hey Joe! </h1>{" "}
+          {UserData.map((userDetail, index) => {
+            return (
+              <div>
+                <Table celled padded>
+                  <Table.Header>
+                    <Table.Row>
+                      <Table.HeaderCell singleLine>
+                        {" "}
+                        Evidence Rating{" "}
+                      </Table.HeaderCell>{" "}
+                      <Table.HeaderCell> ID </Table.HeaderCell>{" "}
+                      <Table.HeaderCell> NAME </Table.HeaderCell>{" "}
+                      <Table.HeaderCell> HASHED EMAIL </Table.HeaderCell>{" "}
+                      <Table.HeaderCell> LAST LOCATION </Table.HeaderCell>{" "}
+                    </Table.Row>{" "}
+                  </Table.Header>
+                  <Table.Body>
+                    <Table.Row>
+                      <Table.Cell>
+                        <Header as="h2" textAlign="center">
+                          {" "}
+                          {userDetail.risk}{" "}
+                        </Header>{" "}
+                      </Table.Cell>{" "}
+                      <Table.Cell singleLine> {userDetail.id} </Table.Cell>{" "}
+                      <Table.Cell>
+                        {userDetail.name}
+                        {/* <Rating icon="star" defaultRating={3} maxRating={3} />{" "} */}
+                      </Table.Cell>{" "}
+                      <Table.Cell textAlign="right">
+                        <br /> {userDetail.email_md5}{" "}
+                      </Table.Cell>{" "}
+                      <Table.Cell>
+                        {userDetail.last_location.ip}
+                        <a href="#"> 18 studies </a>{" "}
+                      </Table.Cell>{" "}
+                    </Table.Row>{" "}
+                  </Table.Body>{" "}
+                </Table>{" "}
+              </div>
+            );
+          })}
+          }{" "}
+        </Container>{" "}
       </div>
     );
   }
